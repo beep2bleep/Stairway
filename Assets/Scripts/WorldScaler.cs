afa65a382eb630a33722e1cdf0203d4d176e3b7a
Mark S <stick100@hotmail.com>
2016-01-29 23:44:36 -0500

    full forward

---
+++ Assets/Scripts/WorldScaler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WorldScaler : MonoBehaviour
+{
+    [SerializeField]
+    private Transform gameCameraTfm = Camera.main.transform;
+
+    private Transform tfm = null;
+    
+	private void LateUpdate()
+	{
+        if (this.tfm == null)
+        {
+            this.tfm = this.GetComponent<Transform>();
+        }
+
+        this.tfm.localScale = new Vector3(1, 1 / Mathf.Cos(this.gameCameraTfm.eulerAngles.x * Mathf.Deg2Rad), 1 / Mathf.Sin(this.gameCameraTfm.eulerAngles.x * Mathf.Deg2Rad));
+	}
+}
