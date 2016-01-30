0bfc652f67c423a5ba6753c4c52fc90acb921a4e
Mark S <stick100@hotmail.com>
2016-01-30 13:58:51 -0500

    walking, spawning, random enemy coloring.

---
+++ Assets/randomColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomColor : MonoBehaviour {
+
+    public SpriteRenderer walk1;
+    public SpriteRenderer walk2;
+    public SpriteRenderer death;
+
+    // Use this for initialization
+    void Start () {
+        walk1.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        walk2.material.color = walk1.material.color;
+        death.material.color = walk1.material.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
