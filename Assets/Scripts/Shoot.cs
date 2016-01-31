41992001538d398410d5c550037a95463bb6afe7
Mark S <stick100@hotmail.com>
2016-01-30 22:29:03 -0500

    got gun and guitar sprite working

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -17,6 +17,8 @@ public class Shoot : MonoBehaviour
     public bool touchShot = false;
     public Transform gun;
     private Transform prefabHolder;
+    public SpriteRenderer guitar;
+    public SpriteRenderer spriteGun;
 
     // Use this for initialization
     void Start()
@@ -58,11 +60,13 @@ public class Shoot : MonoBehaviour
 				this.audioS.PlayOneShot(Audio);
 			}
             stairWay.volume = 0;
+            spriteGun.enabled = true;
             Destroy(clone, .8f);
 		}
 		else if(lastfiretime > timebetweenshots && (Input.GetButton("Fire2") || Input.GetAxisRaw("LeftTrigger") != 0) == true)//(false)//Test for touchscreen input
 		{
             //Alt fire (music)
+            spriteGun.enabled = false;
             lastfiretime = 0;
             //var clone = Instantiate(Bullet, gun.position, gun.rotation);
 
@@ -70,15 +74,19 @@ public class Shoot : MonoBehaviour
             {
                 lastSoundtime = 0;
                 //stairWay.pitch = Random.Range(.5f,2.5f);
-                if(stairWay.volume != 1)
-                    stairWay.volume = 1;
+                if (stairWay.volume != 1) { stairWay.volume = 1; }
+                guitar.enabled = true;
             }
             //Destroy(clone, .8f);
         }
         else
         {
             if (!(Input.GetButton("Fire2") || Input.GetAxisRaw("LeftTrigger") != 0) == true)
+            {
                 stairWay.volume = 0;
+                guitar.enabled = false;
+                
+            }
         }
         
 #endif
