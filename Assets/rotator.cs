47563319d661f6bfaa57fedb097630213386952e
Mark S <stick100@hotmail.com>
2016-01-31 14:23:54 -0500

    Spider march on start

---
+++ Assets/rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = new Quaternion(transform.rotation.x+.01f, transform.rotation.y + .01f, transform.rotation.z + .01f, transform.rotation.w+.01f);// transform.Rotate(new Vector3(1, 1, 1));
+	}
+}
